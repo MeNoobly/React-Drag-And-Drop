226f7694df922662092b60c4ee9154ae3f2e4792
MeNoobly <84956743974@mail.ru>
2023-03-18 17:49:37 +0300

    start learn drag-and-drop

---
+++ src/components/Cards.jsx
@@ -0,0 +1,73 @@
+﻿import React, { useState } from 'react';
+import styles from "../css/card.module.css";
+
+const Cards = () => {
+    const [cardList, setCardList] = useState([
+        {id: 1, order: 3, text: "Карточка 3"},
+        {id: 2, order: 1, text: "Карточка 1"},
+        {id: 3, order: 2, text: "Карточка 2"},
+        {id: 4, order: 4, text: "Карточка 4"},
+      ]);
+    
+      const [currentCard, setCurrentCard] = useState(null);
+    
+      const dragStartHandler = (event, card) => {
+        setCurrentCard(card);
+      }
+    
+      const dragLeaveHandler = (event) => {
+        event.target.style.background = "white";
+      }
+    
+      const dragEndHandler = (event) => {
+        event.target.style.background = "white";
+      }
+    
+      const dragOverHandler = (event) => {
+        event.preventDefault();
+        event.target.style.background = "lightgray";
+      }
+    
+      const dropHandler = (event, card) => {
+        event.preventDefault();
+        setCardList(cardList.map(element => {
+          if (element.id === card.id) {
+            return {...element, order: currentCard.order}
+          }
+          if (element.id === currentCard.id) {
+            return {...element, order: card.order}
+          }
+          return element;
+        }))
+        event.target.style.background = "white";
+      }
+    
+      const sortCards = (a, b) => {
+        if (a.order > b.order) {
+          return 1;
+        } else {
+          return -1;
+        }
+      }
+    
+      return (
+        <div className={styles.App}>
+          {cardList.sort(sortCards).map(card => 
+            <div
+              onDragStart={(event) => dragStartHandler(event, card)}
+              onDragLeave={(event) => dragLeaveHandler(event)}
+              onDragEnd={(event) => dragEndHandler(event)}
+              onDragOver={(event) => dragOverHandler(event)}
+              onDrop={(event) => dropHandler(event, card)}
+              draggable={true}
+              className={styles.Card}
+              key={card.id}
+            > 
+              {card.text}
+            </div>
+          )}
+        </div>
+      );
+};
+
+export default Cards;
---
+++ src/components/Trello.jsx
@@ -0,0 +1,29 @@
+﻿import React, { useState } from 'react';
+import styles from "../css/tables.module.css"
+
+const Tables = () => {
+    const [boards, setBoards] = useState([
+        {id: 1, title: "Сделать", items: [{id: 1, title: "Пойти в магазин"}]},
+        {id: 2, title: "Сделать", items: [{id: 1, title: "Пойти в магазин"}]},
+        {id: 3, title: "Сделать", items: [{id: 1, title: "Пойти в магазин"}]}
+    ]);
+    return (
+        <div className={styles.App}>
+            {boards.map(board => 
+                <div className={styles.Board}>
+                    <div className={styles.BoardTitle}>{board.title}</div>
+                    {board.items.map(item => 
+                        <div
+                            draggable={true}
+                            className={styles.Item}
+                        >
+                        {item.title}
+                        </div>
+                    )}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Tables;
